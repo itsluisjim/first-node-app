4056baac63788549fe4714d1c77d3610a7df61d1
Luis <eternal_pitta0b@icloud.com>
2024-02-23 15:51:20 -0600

    use express to create routes

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-const http = require('node:http');
-const fs = require('fs');
+const path = require("node:path");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const path = req.url;
+const homePath = /^\/(home)?$/;
 
-    let file = '';
+app.get(homePath, function (req, res) {
+  if (res.statusCode == 404) {
+    return res.sendFile(path.join(__dirname, "404.html"));
+  }
 
-    switch(path){
-        case '/':
-            file = 'index.html';
-            break;
-        case '/about':
-            file = 'about.html';   
-            break;
-        case '/contact':
-            file = 'contact.html';
-            break;
-        default:
-            file = '404.html';
-    }
+  return res.sendFile(path.join(__dirname, "home.html"));
+});
 
+app.get("/about", function (req, res) {
+  if (res.statusCode == 404) {
+    return res.sendFile(path.join(__dirname, "404.html"));
+  }
 
-    fs.readFile(file, function(err, data) {
-        if(err){
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("Something is really broken.");
-        }
+  return res.sendFile(path.join(__dirname, "about.html"));
+});
 
-        if(file == '404.html'){
-            res.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-        }
-        res.write(data);
-        return res.end();
-      });
+app.get("/contact", function (req, res) {
+  if (res.statusCode == 404) {
+    return res.sendFile(path.join(__dirname, "404.html"));
+  }
+  return res.sendFile(path.join(__dirname, "contact.html"));
+});
 
-})
+app.get("*", function (req, res) {
+  res.status(404);
+  return res.sendFile(path.join(__dirname, "404.html"));
+});
 
-server.listen(8000);
+app.listen(8080);
